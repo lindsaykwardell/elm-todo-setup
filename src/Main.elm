241ed5fe1355506dcbcb51eb9d1c59f554e1c5fc
Lindsay Wardell <three060@gmail.com>
2022-07-15 09:00:33 -0700

    Make todo app work

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (main)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onCheck, onClick, onInput)
 
 
 type alias Model =
@@ -11,12 +12,16 @@ type alias Model =
 
 
 type alias Todo =
-    { task : String
+    { id : Int
+    , task : String
+    , completed : Bool
     }
 
 
 type Msg
-    = NoOp
+    = ToggleTodoComplete Todo Bool
+    | UpdateTodoText Todo String
+    | AddTodo
 
 
 main : Program () Model Msg
@@ -27,11 +32,53 @@ main =
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        NoOp ->
-            model
+        ToggleTodoComplete todo completed ->
+            { model
+                | todos =
+                    List.map
+                        (\t ->
+                            if t.id == todo.id then
+                                { todo | completed = completed }
+
+                            else
+                                t
+                        )
+                        model.todos
+            }
+
+        UpdateTodoText todo task ->
+            { model
+                | todos =
+                    List.map
+                        (\t ->
+                            if t.id == todo.id then
+                                { todo | task = task }
+
+                            else
+                                t
+                        )
+                        model.todos
+            }
+
+        AddTodo ->
+            { model | todos = model.todos ++ [ { id = List.length model.todos + 1, task = "", completed = False } ] }
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ h1 [] [ text "Todos" ] ]
+    div [ style "width" "200px", style "margin" "auto"]
+        [ h1 [] [ text "Todos" ]
+        , main_ []
+            [ div []
+                (model.todos
+                    |> List.map
+                        (\todo ->
+                            div [ style "display" "flex" ]
+                                [ input [ type_ "checkbox", onCheck (ToggleTodoComplete todo) ] []
+                                , input [ value todo.task, onInput (UpdateTodoText todo) ] []
+                                ]
+                        )
+                )
+            , button [ onClick AddTodo ] [ text "Add todo" ]
+            ]
+        ]
