3d54053a1cb41a2a5978708fb15880a6d852d78e
Lindsay Wardell <three060@gmail.com>
2022-07-15 09:36:01 -0700

    Make it pretty

--- src/Main.elm
+++ src/Main.elm
@@ -3,11 +3,13 @@ module Main exposing (main)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onCheck, onClick, onInput)
+import Html.Events exposing (onCheck, onClick, onInput, onSubmit)
 
 
 type alias Model =
     { todos : List Todo
+    , newTodo : String
+    , filter : TodoFilter
     }
 
 
@@ -19,19 +21,39 @@ type alias Todo =
 
 
 type Msg
-    = ToggleTodoComplete Todo Bool
+    = UpdateNewTodoText String
+    | ToggleTodoComplete Todo Bool
     | UpdateTodoText Todo String
     | AddTodo
+    | DeleteTodo Todo
+    | UpdateFilter TodoFilter
+
+
+type TodoFilter
+    = All
+    | Active
+    | Completed
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox { init = { todos = [] }, update = update, view = view }
+    Browser.sandbox
+        { init =
+            { todos = []
+            , newTodo = ""
+            , filter = All
+            }
+        , update = update
+        , view = view
+        }
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
+        UpdateNewTodoText text ->
+            { model | newTodo = text }
+
         ToggleTodoComplete todo completed ->
             { model
                 | todos =
@@ -61,24 +83,89 @@ update msg model =
             }
 
         AddTodo ->
-            { model | todos = model.todos ++ [ { id = List.length model.todos + 1, task = "", completed = False } ] }
+            { model
+                | todos =
+                    model.todos
+                        ++ [ { id = List.length model.todos + 1
+                             , task = model.newTodo
+                             , completed = False
+                             }
+                           ]
+                , newTodo = ""
+            }
+
+        DeleteTodo todo ->
+            { model | todos = List.filter (.id >> (/=) todo.id) model.todos }
+
+        UpdateFilter filter ->
+            { model | filter = filter }
 
 
 view : Model -> Html Msg
 view model =
-    div [ style "width" "200px", style "margin" "auto"]
+    div [ style "width" "200px", style "margin" "auto" ]
         [ h1 [] [ text "Todos" ]
         , main_ []
             [ div []
                 (model.todos
-                    |> List.map
+                    |> List.filterMap
                         (\todo ->
-                            div [ style "display" "flex" ]
-                                [ input [ type_ "checkbox", onCheck (ToggleTodoComplete todo) ] []
-                                , input [ value todo.task, onInput (UpdateTodoText todo) ] []
-                                ]
+                            let
+                                render =
+                                    Just <|
+                                        div [ style "display" "flex" ]
+                                            [ input
+                                                [ type_ "checkbox"
+                                                , checked todo.completed
+                                                , onCheck (ToggleTodoComplete todo)
+                                                ]
+                                                []
+                                            , input
+                                                [ style "flex-grow" "1"
+                                                , value todo.task
+                                                , onInput (UpdateTodoText todo)
+                                                ]
+                                                []
+                                            , button [ onClick (DeleteTodo todo) ] [ text "X" ]
+                                            ]
+                            in
+                            case model.filter of
+                                All ->
+                                    render
+
+                                Active ->
+                                    if not todo.completed then
+                                        render
+
+                                    else
+                                        Nothing
+
+                                Completed ->
+                                    if todo.completed then
+                                        render
+
+                                    else
+                                        Nothing
                         )
                 )
-            , button [ onClick AddTodo ] [ text "Add todo" ]
+            , Html.form
+                [ onSubmit AddTodo ]
+                [ input [ value model.newTodo, onInput UpdateNewTodoText ] []
+                , button [] [ text "Add todo" ]
+                ]
+            , div [ style "display" "flex", style "justify-content" "space-between", style "padding-top" "15px" ]
+                [ button [ activeFilterStyle model.filter All, onClick (UpdateFilter All) ] [ text "All" ]
+                , button [ activeFilterStyle model.filter Active, onClick (UpdateFilter Active) ] [ text "Active" ]
+                , button [ activeFilterStyle model.filter Completed, onClick (UpdateFilter Completed) ] [ text "Completed" ]
+                ]
             ]
         ]
+
+
+activeFilterStyle : TodoFilter -> TodoFilter -> Attribute msg
+activeFilterStyle activeFilter filter =
+    if activeFilter == filter then
+        style "background" "lightblue"
+
+    else
+        style "background" "transparent"
